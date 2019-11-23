5659c9aad66e54f67266cfc7232b497c537942bf
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-23 18:21:57 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,5 @@
 require_relative "../lib/greeting.rb"
 # code the #greeting method here!
 def greeting(name)
-    puts "Hello #{name.chomp}. It's nice to meet you."
+    puts "Hello #{name.strip}. It's nice to meet you."
 end
