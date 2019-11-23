e8eda6341b6313cdb6f95389f252774430fa972b
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-23 18:27:57 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,7 @@
+require_relative "../lib/greeting.rb"
 # code the #greeting method here!
+def greeting(name)
+    puts "Hello #{name.strip}. It's nice to meet you."
+end
+
+puts "Hi! I'm HAL, what's your name?"
